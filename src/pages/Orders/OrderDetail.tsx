b5dafda40546d68c868f5485dd3db5ba49fbf90d
Ana Camila Santos Sousa <acamilass@gmail.com>
2021-08-30 12:27:23 -0300

    Criando componente de detalhe do pedido

---
+++ src/pages/Orders/OrderDetail.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from "../../components/Container/Container";
+
+export function OrderDetail() {
+  return (
+    <Container>
+      <h1>OrderDetail component</h1>
+    </Container>
+  )
+}
