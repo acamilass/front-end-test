a2ddffcd79b72a667e96e72da719654a7154f45f
Ana Camila Santos Sousa <acamilass@gmail.com>
2021-08-31 21:14:39 -0300

    Adicionar icones a barra de progresso

--- src/pages/Orders/components/OrderDetailCard/OrderDetailCard.tsx
+++ src/pages/Orders/components/OrderDetailCard/OrderDetailCard.tsx
@@ -1,6 +1,11 @@
 import React, { PropsWithChildren } from 'react';
 import { toCurrency } from '../../../../core/number';
 import classes from './OrderDetailCard.module.scss';
+import icon_1 from '../../../../images/icon_1.png';
+import icon_2 from '../../../../images/icon_2.png';
+import icon_3 from '../../../../images/icon_3.png';
+import icon_4 from '../../../../images/icon_4.png';
+import icon_5 from '../../../../images/icon_5.png';
 
 export function OrderDetailContainer({ children }: PropsWithChildren<{}>) {
   return <div className={classes.container}>{children}</div>
@@ -10,14 +15,14 @@ export function OrderDetailCard({ props }: any) {
 
   function isSelectedIcon(status: string) {
     return props.status === status
-    ? `${classes.selected_icon} ${classes.status__icon}`
-    : `${classes.status__icon}`
+      ? `${classes.selected_icon} ${classes.status__icon}`
+      : `${classes.status__icon}`
   }
 
   function isSelectedTitle(status: string) {
     return props.status === status
-    ? `${classes.selected} ${classes.status__title}`
-    : `${classes.status__title}`
+      ? `${classes.selected} ${classes.status__title}`
+      : `${classes.status__title}`
   }
 
   return (
@@ -60,7 +65,7 @@ export function OrderDetailCard({ props }: any) {
             {/* <p>{props.status}</p> */}
             <div className={classes.status}>
               <div className={isSelectedIcon('Aguardando pagamento')}>
-
+                <img src={icon_1} />
               </div>
               <span className={isSelectedTitle('Aguardando pagamento')}>
                 Aguardando pagamento
@@ -69,7 +74,7 @@ export function OrderDetailCard({ props }: any) {
 
             <div className={classes.status}>
               <div className={isSelectedIcon('Pagamento aprovado')}>
-
+                <img src={icon_2} />
               </div>
               <span className={isSelectedTitle('Pagamento aprovado')}>
                 Pagamento aprovado
@@ -78,7 +83,7 @@ export function OrderDetailCard({ props }: any) {
 
             <div className={classes.status}>
               <div className={isSelectedIcon('Pedido em separação')}>
-
+                <img src={icon_3} />
               </div>
               <span className={isSelectedTitle('Pedido em separação')}>
                 Pedido em separação
@@ -87,7 +92,7 @@ export function OrderDetailCard({ props }: any) {
 
             <div className={classes.status}>
               <div className={isSelectedIcon('Pedido em transporte')}>
-
+                <img src={icon_4} />
               </div>
               <span className={isSelectedTitle('Pedido em transporte')}>
                 Pedido em transporte
@@ -96,7 +101,7 @@ export function OrderDetailCard({ props }: any) {
 
             <div className={classes.status}>
               <div className={isSelectedIcon('Pedido entregue')}>
-
+                <img src={icon_5} />
               </div>
               <span className={isSelectedTitle('Pedido entregue')}>
                 Pedido entregue
